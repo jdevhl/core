0ae04516c4c99cf9b104c3bdc30539e9cd39ba04
oscarmts <16204530+oscarmts@users.noreply.github.com>
2022-03-07 12:01:41 +0100

    Add XMLParser

---
+++ src/main/java/com/github/jdevhl/parser/XMLParser.java
@@ -0,0 +1,9 @@
+package com.github.jdevhl.parser;
+
+public class XMLParser {
+    private static final String S_SOAP_ENV_SOAP_S_REGEX = "\\s*<\\/?(?:SOAP-ENV|soap):(?:.|\\s)*?>";
+
+    public static String removeXmlSoapEnvelopeAndBody(String xml) {
+        return xml.replaceAll(S_SOAP_ENV_SOAP_S_REGEX, "");
+    }
+}
