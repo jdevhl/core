4b3cdf9f135c7e92940f8ecc8b94e3317eb62b6d
oscarmts <16204530+oscarmts@users.noreply.github.com>
2022-03-09 15:24:54 +0100

    Add EasyMap

---
+++ src/main/java/jdevhl/util/EasyMap.java
@@ -0,0 +1,44 @@
+package jdevhl.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EasyMap {
+
+    public static boolean isEmpty(Map<?, ?> value) {
+        if (value == null) {
+            return false;
+        }
+        return value.size() == 0;
+    }
+
+    public static boolean isNotEmpty(Map<?, ?> value) {
+        return !isEmpty(value);
+    }
+
+    public static Map<?, ?> sort(Map<?, ?> map) {
+        if (isEmpty(map)) {
+            return map;
+        }
+        checkMapComparableValues(map);
+        final Map<Comparable, Comparable> mabObj = (Map<Comparable, Comparable>) map;
+        return mabObj.entrySet().stream().sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+    }
+
+    private static void checkMapComparableValues(Map<?, ?> map) {
+        final Object key = map.keySet().stream().findFirst().get();
+        if (!isComparable(key)) {
+            throw new IllegalArgumentException("Key must be Comparable");
+        }
+        if (!isComparable(map.get(key))) {
+            throw new IllegalArgumentException("Value must be Comparable");
+        }
+    }
+
+    private static boolean isComparable(Object key) {
+        return key instanceof Comparable;
+    }
+}
